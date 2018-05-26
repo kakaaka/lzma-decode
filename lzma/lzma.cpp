885a3a6c8487ac7259c2c9fa347259c048d39d86
海贼猎人 <dongch007@gmail.com>
2018-05-26 20:40:56 +0800

    修改工程目录

--- lzma/lzma.cpp
+++ lzma/lzma.cpp
@@ -1,11 +1,17 @@
 #include "7zip\LzmaDec.h"
 #include "7zip\Alloc.h"
 
+#ifdef  _WIN32
+#define EXPORT_API __declspec(dllexport)
+#else
+#define EXPORT_API 
+#endif
+
 #define LZMA_PROPS_SIZE 5
 
 extern "C"
 {
-	__declspec(dllexport) int LzmaUncompress(Byte* src, SizeT srcLen, Byte* dest, SizeT destLen)
+	EXPORT_API int LzmaUncompress(Byte* src, SizeT srcLen, Byte* dest, SizeT destLen)
 	{
 		ELzmaStatus status;
 		return LzmaDecode(dest, &destLen, src + LZMA_PROPS_SIZE + 8, &srcLen, src, LZMA_PROPS_SIZE, LZMA_FINISH_ANY, &status, &g_Alloc);
